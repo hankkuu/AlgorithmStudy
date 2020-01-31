f25fc7a269ce0795e1c50d506bc663c64605fe7d
hankkuu <hankkuu@gmail.com>
2020-02-01 02:41:10 +0900

    study_20200204

---
+++ study1_200204/Factorial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AlgorithmStudy
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("plz enter a number from 0 to 15\n");
+                Console.Write("input : ");
+                try
+                {
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    if (n > 0 && n < 15)
+                    {
+                        Console.WriteLine("Factorial : " + Factorial(n));
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("the number is out of range ");
+                    }
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+            Console.WriteLine("end");
+        }
+
+        static int Factorial(int i)
+        {
+            if (i == 1)
+                return 1;
+
+            return i * Factorial(i - 1);
+        }
+    }
+}
